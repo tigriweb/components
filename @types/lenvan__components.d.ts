39e7aa8903c5dee3c08944be286bd0d4b8e053fc
lenvan <lenvanscripts@gmail.com>
2024-10-16 17:24:20 +0200

    FEAT: INIT

--- @types/lenvan__components.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@lenvan/components/primitives' {
-	export const TwebSVG: (props: { icon: string, size?: number, stroke?: number }) => JSX.Element;
-}
--- index.js
+++
@@ -1,4 +0,0 @@
-export * from './attributes';
-export * from './controls';
-export * from './meta';
-export * from './primitives';
