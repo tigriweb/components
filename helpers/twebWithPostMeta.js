8d7fc172fe7f26e91124d18ac59ce920fedc0609
lenvan <lenvanscripts@gmail.com>
2024-10-20 08:13:42 +0200

    FEAT: update twebWithPostMeta

--- helpers/twebWithPostMeta.js
+++ helpers/twebWithPostMeta.js
@@ -3,25 +3,69 @@ import {
 } from '@wordpress/compose';
 
 import {
+	useEntityProp
+} from '@wordpress/core-data';
+
+import {
+	useCallback,
+	useMemo
+} from '@wordpress/element';
+
+import {
 	withDispatch,
 	withSelect,
 } from '@wordpress/data';
 
-const twebWithPostMeta = WrappedComponent => {
-	return compose(
-		withDispatch((dispatch, props) => ({
-			setMetaValue: value => {
-				dispatch('core/editor').editPost({
-					meta: {
-						[props.metaKey]: value,
-					},
-				});
-			},
-		})),
-		withSelect((select, props) => ({
-			metaValue: select('core/editor').getEditedPostAttribute('meta')[props.metaKey],
-		}))
-	)(WrappedComponent);
-};
+const twebWithPostMeta = (WrappedComponent) => {
+	return (props) => {
+		const { metaKey, isNewProductEditor } = props;
 
+		// New WooCommerce Product Editor
+		if (isNewProductEditor) {
+			const [ metadata, setMetadata ] = useEntityProp( 'postType', 'product', 'meta_data' );
+
+			const metadataItem = useMemo(
+				() => metadata?.find( ( item ) => item.key === metaKey ) || null,
+				[ metadata, metaKey ]
+			);
+
+			const setMetaValue = useCallback(
+				( newValue ) => {
+					if ( !metadataItem ) {
+						setMetadata( [ ...metadata, { key: metaKey, value: newValue } ] );
+					} else {
+						setMetadata(
+							metadata.map( ( item ) =>
+								item.key === metaKey ? { ...item, value: newValue } : item
+							)
+						);
+					}
+				},
+				[ metadata, metaKey, metadataItem ]
+			);
+
+			const metaValue = metadataItem?.value ?? '';
+
+			return (
+				<WrappedComponent
+					{...props}
+					metaValue={metaValue}
+					setMetaValue={setMetaValue}
+				/>
+			);
+		}
+
+		// Gutenberg
+		return compose(
+			withDispatch((dispatch) => ({
+				setMetaValue: (value) => {
+					dispatch('core/editor').editPost({ meta: { [metaKey]: value } });
+				},
+			})),
+			withSelect((select) => ({
+				metaValue: select('core/editor').getEditedPostAttribute('meta')[metaKey],
+			}))
+		)(WrappedComponent)(props);
+	};
+};
 export default twebWithPostMeta;
